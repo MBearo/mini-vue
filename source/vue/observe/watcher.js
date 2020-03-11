b4346c9c01b6109deb37041e910633da13df7e5a
MBearo <295290708@qq.com>
2020-03-11 12:05:06 +0800

    UPG:异步更新

--- source/vue/observe/watcher.js
+++ source/vue/observe/watcher.js
@@ -30,6 +30,10 @@ class Watcher {
   }
 
   update () {
+    queueWatcher(this)
+  }
+
+  run () {
     this.get()
   }
 
@@ -43,5 +47,44 @@ class Watcher {
       dep.addSub(this)
     }
   }
+}
+
+let has = {}
+let queue = []
+function flushQueue () {
+  queue.forEach(watcher => watcher.run())
+  has = {}
+  queue = []
+}
+function queueWatcher (watcher) {
+  const id = watcher.id
+  if (!has[id]) {
+    has[id] = true
+    queue.push(watcher)
+  }
+  nextTick(flushQueue)
+}
+const callbacks = []
+function flushCallbacks () {
+  callbacks.forEach(cb => cb())
+}
+function nextTick (cb) {
+  callbacks.push(cb)
+  const timerFunc = () => {
+    flushCallbacks()
+  }
+  if (Promise) {
+    return Promise.resolve().then(timerFunc)
+  }
+  if (MutationObserver) {
+    const observe = new MutationObserver(timerFunc)
+    const textNode = document.createTextNode(1)
+    observe.observe(textNode, { characterData: true })
+    textNode.textContent(2)
+  }
+  if (setImmediate) {
+    return setImmediate(timerFunc)
+  }
+  setTimeout(timerFunc, 0)
 }
 export default Watcher
